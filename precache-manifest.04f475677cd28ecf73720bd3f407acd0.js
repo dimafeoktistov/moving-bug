1364cb3085863d7cc92976e54315ee6e2ad9642f
Dmitriy Feoktistov <feoktistovdmitriy@gmail.com>
2019-02-13 04:21:18 +0700

    Updates

---
+++ precache-manifest.04f475677cd28ecf73720bd3f407acd0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "daedbf72569b8da189f7e09b1295e461",
+    "url": "/moving-bug/static/media/insect.daedbf72.svg"
+  },
+  {
+    "revision": "e4ae1af1bd92ae68bde4d1d22d2effd0",
+    "url": "/moving-bug/static/media/bug3.e4ae1af1.svg"
+  },
+  {
+    "revision": "33845da3d9a6ae3ab033b634a3e9a76f",
+    "url": "/moving-bug/static/media/bug2.33845da3.svg"
+  },
+  {
+    "revision": "835616ade78b18c6c6ea044ed85763a6",
+    "url": "/moving-bug/static/media/bug1.835616ad.svg"
+  },
+  {
+    "revision": "9b51008ff40676b8146fae71d4889810",
+    "url": "/moving-bug/static/media/bug.9b51008f.svg"
+  },
+  {
+    "revision": "892d7e9b63d4d9307fc0",
+    "url": "/moving-bug/static/js/runtime~main.892d7e9b.js"
+  },
+  {
+    "revision": "3e643e1be607dd22f3a0",
+    "url": "/moving-bug/static/js/main.3e643e1b.chunk.js"
+  },
+  {
+    "revision": "acfa5b4d1d46c5668c37",
+    "url": "/moving-bug/static/js/1.acfa5b4d.chunk.js"
+  },
+  {
+    "revision": "3e643e1be607dd22f3a0",
+    "url": "/moving-bug/static/css/main.a19ae3ad.chunk.css"
+  },
+  {
+    "revision": "92a49341d224b0585e2294a607344e9a",
+    "url": "/moving-bug/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/moving-bug/precache-manifest.04f475677cd28ecf73720bd3f407acd0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/moving-bug/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
