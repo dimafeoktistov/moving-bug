a1c1df01bdc00e114259fb27666a4aa259c11afa
Dmitriy Feoktistov <feoktistovdmitriy@gmail.com>
2019-02-13 04:32:19 +0700

    fix: fixed identation and formatting

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,44 +1,41 @@
-import * as d3 from "d3-ease";
+import * as d3 from 'd3-ease';
 
 export const getEasingFunc = (name, t) => {
   switch (name) {
-    case "circleIn":
+    case 'circleIn':
       return d3.easeCircleIn(t);
 
-    case "expIn":
+    case 'expIn':
       return d3.easeExpIn(t);
 
-    case "expOut":
+    case 'expOut':
       return d3.easeExpOut(t);
 
-    case "bounceIn":
+    case 'bounceIn':
       return d3.easeBounceIn(t);
 
-    case "bounceOut":
+    case 'bounceOut':
       return d3.easeBounceOut(t);
 
-    case "elasticOut":
+    case 'elasticOut':
       return d3.easeElasticOut(t);
 
     default:
-      break;
+      throw new Error('Такой функции нет');
   }
 };
 
-export const getElemPosition = el => {
+export const getElemPosition = (el) => {
   const { top, left } = el.current.getBoundingClientRect();
 
   return {
     x: left,
-    y: top
+    y: top,
   };
 };
 
-export const generateRandomValue = (min, max) =>
-  Math.floor(Math.random() * (max - min) + min);
+export const generateRandomValue = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
-export const getRotationDegrees = (anchor, point) =>
-  (Math.atan2(anchor.y - point.y, anchor.x - point.x) * 180) / Math.PI + 270;
+export const getRotationDegrees = (anchor, point) => (Math.atan2(anchor.y - point.y, anchor.x - point.x) * 180) / Math.PI + 270;
 
-export const translate = (x, y, angle) =>
-  `translate(${x}px, ${y}px) rotate(${angle}deg)`;
+export const translate = (x, y, angle) => `translate(${x}px, ${y}px) rotate(${angle}deg)`;
